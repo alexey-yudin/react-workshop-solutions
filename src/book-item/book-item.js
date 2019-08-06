e88b01680c859ec38363e664a3b12d89c4ec507e
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-06 10:31:00 +0200

    Expand and collapse text.

--- src/book-item/book-item.js
+++ src/book-item/book-item.js
@@ -4,6 +4,18 @@ import {BookImage} from '../book-image/book-image';
 import './book-item.css';
 
 export class BookItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isExpanded: false
+    };
+  }
+
+  toggleText = () => {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  };
+
   render() {
     return (
       <div className='book-item'>
@@ -13,8 +25,16 @@ export class BookItem extends Component {
 
         <div className='book-item__content'>
           <h1 className='book-item__title'>{this.props.title}</h1>
+
           <p className='book-item__text'>
-            {this.props.text}
+            {
+              this.state.isExpanded
+                ? this.props.text
+                : `${this.props.text.slice(0, 10)}...`
+            }
+            <button type='button' onClick={this.toggleText}>
+              {this.state.isExpanded ? 'Collapse' : 'Expand'}
+            </button>
           </p>
         </div>
       </div>
